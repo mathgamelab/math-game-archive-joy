1e0eccf34c9ec892abe7dbd721253fded3b2cbde
Happy_YoonSSAM <why-math@naver.com>
2026-01-01 22:22:02 +0900

    Update vite.config.ts

--- apps/edugame_builder/vite.config.ts
+++ apps/edugame_builder/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '../../', '');
     return {
       root: __dirname,
+      base: '/edugame_builder/',
       server: {
         port: 3000,
         host: '0.0.0.0',
